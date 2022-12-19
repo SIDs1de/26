bcedc51b94fc86e820c0b95a3c3f5e5181fbfdfc
SIDs1de <igorekdenisenkov@yandex.ru>
2022-12-19 21:07:04 +0300

    добавил бургер меню(адаптировал шапку)

--- src/js/main.js
+++ src/js/main.js
@@ -30,9 +30,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const activateScrollBtns = () => {
+    const burgerBtn = document.querySelector('.burger')
+    const headerMenu = document.querySelector('.header__menu')
+    const body = document.querySelector('.page')
+
     document.querySelectorAll('a[href^="#"').forEach(link => {
       link.addEventListener('click', function (e) {
         e.preventDefault()
+
+        headerMenu.classList.remove('_open')
+        body.classList.remove('_fixed')
+        if (burgerBtn.classList.contains('_open')) {
+          burgerBtn.classList.replace('_open', '_close')
+        }
 
         const href = this.getAttribute('href').substring(1)
 
@@ -49,6 +59,35 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+  const openCloseMenu = (burgerBtn, headerMenu, body) => {
+    if (!headerMenu.classList.contains('_open')) {
+    }
+    if (burgerBtn.classList.contains('_open')) {
+      burgerBtn.classList.replace('_open', '_close')
+    } else if (burgerBtn.classList.contains('_close')) {
+      burgerBtn.classList.replace('_close', '_open')
+    } else {
+      burgerBtn.classList.add('_open')
+    }
+
+    headerMenu.classList.toggle('_open')
+    body.classList.toggle('_fixed')
+  }
+
+  const burger = () => {
+    const burgerBtn = document.querySelector('.burger')
+    const headerMenu = document.querySelector('.header__menu')
+    const body = document.querySelector('.page')
+
+    burgerBtn.addEventListener('click', () => {
+      window.scrollTo({
+        top: 0
+      })
+      openCloseMenu(burgerBtn, headerMenu, body)
+    })
+  }
+
+  burger()
   activateSliders()
   activateScrollBtns()
 })
